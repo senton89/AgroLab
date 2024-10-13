5fdb34c36cda5801b62351736149883fa783e21f
senton89 <157795750+senton89@users.noreply.github.com>
2024-10-13 18:26:02 +0300

    Main form visual part

--- AgroLab/obj/Debug/net8.0-windows/App.g.cs
+++ AgroLab/obj/Debug/net8.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace AgroLab {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace AgroLab {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public static void Main() {
             AgroLab.App app = new AgroLab.App();
             app.InitializeComponent();
